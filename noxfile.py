0c2e85a420a48acb1259e3ed866e485cbbc8e605
Lars Hupfeldt <lhn@hupfeldtit.dk>
2020-08-20 18:32:00 +0200

    Initial Aplha release - everything may change :)

---
+++ noxfile.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+import nox
+
+
+_HERE = Path(__file__).absolute().parent
+_TEST_DIR = _HERE/'test'
+_DOC_DIR = _HERE/'doc'
+# Locally we have nox handle the different versions, but in each travis run there is only a single python which can always be found as just 'python'
+_PY_VERSIONS = ['3.9', '3.8', '3.7', '3.6'] if not os.environ.get("TRAVIS_PYTHON_VERSION") else ['python']
+
+
+@nox.session(python=_PY_VERSIONS, reuse_venv=True)
+def test(session):
+    session.install('.')
+    session.install('-r', str(_TEST_DIR/'requirements.txt'))
+    cov_rc_file_name = '.coverage_rc'  # The rcfile path is duplicated in .travis.yml
+    session.run('pytest', '--capture=sys', f'--cov={_HERE}', '--cov-report=term-missing', f'--cov-config={_TEST_DIR/cov_rc_file_name}', *session.posargs)
+
+
+@nox.session(python=_PY_VERSIONS, reuse_venv=True)
+def examples(session):
+    session.install('-e', '.')
+    session.run('python', '-m', 'compileall', str(_HERE/'examples/result.py'))
